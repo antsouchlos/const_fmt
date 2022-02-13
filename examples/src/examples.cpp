4128ede5db22d52913aa67c9b712701032bc83ce
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2022-02-13 19:43:22 +0100

    English

--- examples/src/examples.cpp
+++ examples/src/examples.cpp
@@ -7,7 +7,7 @@ using namespace const_fmt;
 int main() {
     constexpr auto s = "abcdef {:04}"_const_fmt(123);
 
-    // Convert the s (with a type of 'std::array<char, N>') into something
+    // Convert s (with a type of 'std::array<char, N>') into something
     // writable to std::cout
     std::string_view sv{&s[0], s.size()};
 
