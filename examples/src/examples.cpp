3db91aebda4d51814a5d6412f23803b3853ee43e
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2022-02-13 17:53:17 +0100

    Proper example

--- examples/src/examples.cpp
+++ examples/src/examples.cpp
@@ -1,8 +1,17 @@
 #include <const_fmt/format.h>
 #include <iostream>
+#include <string_view>
+
+using namespace const_fmt;
 
 int main() {
-    // TODO
+    constexpr auto s = "abcdef {:04}"_fmt(123);
+
+    // Convert the s (with a type of 'std::array<char, N>') into something
+    // writable to std::cout
+    std::string_view sv{&s[0], s.size()};
+
+    std::cout << sv << std::endl;
 
     return 0;
 }
